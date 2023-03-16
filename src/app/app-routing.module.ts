80813f7223a19d41c4bff167080593242c37fbbf
Tengiz Mikhelidze <mixelidzet@gmail.com>
2023-03-17 01:03:35 +0400

    feat - wifi qr generator

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'wifi'
+      },
+      {
+        path: 'wifi',
+        loadChildren: ()=>
+          import('./wifi-qr/wifi-qr.module').then((m)=>m.WifiQrModule)
+      },
+      {
+        path: 'link',
+        loadChildren: ()=>
+          import('./link-qr/link-qr.module').then((m)=>m.LinkQrModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
